0ec0e4a409cd68698016ff91e2ea84ae07ce753e
aminya <aminyahyaabadi74@gmail.com>
2020-03-24 18:35:28 -0500

    for-array
    - Formatting
    - Tes

--- src_src/for-array.ts
+++ src_src/for-array.ts
@@ -1,87 +1,108 @@
 import { Chance } from "chance"
 const chance = new Chance()
-
 const Benchmark = require("benchmark")
-let suite = new Benchmark.Suite()
-let suite2 = new Benchmark.Suite()
 
 /* ************************************************************************* */
 // setup
 const arr = chance.n(chance.floating, 10000)
-const arr_str = chance.n(chance.string, 10000)
 
 /* ************************************************************************* */
-
-// add tests
-suite.add("for-traditional", function() {
+// functions
+function for_traditional() {
   let sum = 0
   for (let i = 0, l = arr.length; i < l; ++i) {
     sum += arr[i]
   }
   return sum
-})
+}
 
-suite.add("for-of", function() {
+function for_of () {
   let sum = 0
   for (const a of arr) {
     sum += a
   }
   return sum
-})
+}
 
-suite.add("for-in", function() {
+function for_in () {
   let sum = 0
   for (const i in arr) {
     sum += arr[i]
   }
   return sum
-})
+}
+/* ************************************************************************* */
+// test
+console.assert(for_of() === for_in() && for_in() === for_traditional())
 
+let suite = new Benchmark.Suite()
+
+// add benchmarks
+suite.add("for_traditional", () => for_traditional())
+suite.add("for_of", () => for_of())
+suite.add("for_in", () => for_in())
 
 // add listeners
-suite.on("cycle", function(event) {
-  console.log(String(event.target))
-})
-suite.on("complete", function() {
-  console.log("Fastest is " + this.filter("fastest").map("name"))
-})
-// run async
+suite.on("cycle", (event) => console.log(String(event.target)) )
+suite.on("complete", () => console.log("Fastest is " + suite.filter("fastest").map("name")) )
+
+// run benchmark
+console.log("number array")
 suite.run({ async: false })
 
 /* ************************************************************************* */
+// String array
 
-suite2.add("for-traditional-str", function() {
+/* ************************************************************************* */
+// setup
+const arr_str = chance.n(chance.string, 10000)
+
+/* ************************************************************************* */
+// functions
+function for_traditional_str() {
   let sum = ""
   for (let i = 0, l = arr_str.length; i < l; ++i) {
     sum.concat(arr_str[i])
   }
   return sum
-})
+}
 
-suite2.add("for-of-str", function() {
+function for_of_str () {
   let sum = ""
   for (const a of arr_str) {
     sum.concat(a)
   }
   return sum
-})
+}
 
-suite2.add("for-in-str", function() {
+function for_in_str () {
   let sum = ""
   for (const i in arr_str) {
     sum.concat(arr_str[i])
   }
   return sum
-})
+}
+
+/* ************************************************************************* */
+// test
+console.assert(for_of_str() === for_in_str() && for_in_str() === for_traditional_str())
+
+
+let suite2 = new Benchmark.Suite()
+
+// add benchmarks
+suite2.add("for_traditional_str", () => for_traditional_str())
+suite2.add("for_of_str", () => for_of_str())
+suite2.add("for_in_str", () => for_in_str())
 
 // add listeners
-suite2.on("cycle", function(event) {
-  console.log(String(event.target))
-})
-suite2.on("complete", function() {
-  console.log("Fastest is " + this.filter("fastest").map("name"))
-})
-// run async
+suite2.on("cycle", (event) => console.log(String(event.target)) )
+suite2.on("complete", () => console.log("Fastest is " + suite2.filter("fastest").map("name")) )
+
+// run benchmark
+console.log("string array")
 suite2.run({ async: false })
+
+/* ************************************************************************* */
 
 export {};
