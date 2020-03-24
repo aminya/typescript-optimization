c78f8dc020bca82410c0bb335718bf011bd8de13
aminya <aminyahyaabadi74@gmail.com>
2020-03-24 18:49:21 -0500

    for-of
    - Formatting
    - Seed
    - Test

--- src_src/for-of-array.ts
+++ src_src/for-of-array.ts
@@ -1,45 +1,64 @@
 import { Chance } from "chance"
-const chance = new Chance()
+
+const chance = new Chance(12345)
+const chance2 = new Chance(12345)
 
 const Benchmark = require("benchmark")
-let suite = new Benchmark.Suite()
+
+/* ************************************************************************* */
+// parameter
+const arr_length = 10
+
+console.log("array size of "+ arr_length)
+
+/* ************************************************************************* */
+// number array
 
 /* ************************************************************************* */
 // setup
-const arr = chance.n(chance.floating, 10000)
+const arr = chance.n(chance.floating, arr_length)
 
 /* ************************************************************************* */
-// add tests
-suite.add("for-of", function() {
+// functions
+
+function for_of () {
   let sum = 0
   for (const a of arr) {
     sum += a
   }
   return sum
-})
-
-function arr_return() {
-  const arr2: number[] = []
-  for (let i = 0; i < 10000; ++i) arr2.push(i)
-  return arr2
 }
 
-suite.add("for-of-full-lookup", function() {
+
+function arr_return() {
+  return chance2.n(chance2.floating, arr_length)
+}
+
+function for_of_full_lookup () {
   let sum = 0
   for (const a of arr_return()) {
     sum += a
   }
   return sum
-})
+}
+/* ************************************************************************* */
+// test
+console.assert(for_of() === for_of_full_lookup())
+
+let suite = new Benchmark.Suite()
+
+// add benchmarks
+suite.add("for_of", () => for_of())
+suite.add("for_of_full_lookup", () => for_of_full_lookup())
 
 // add listeners
-suite.on("cycle", function(event) {
-  console.log(String(event.target))
-})
-suite.on("complete", function() {
-  console.log("Fastest is " + this.filter("fastest").map("name"))
-})
-// run async
-suite.run({ async: true })
+suite.on("cycle", (event) => console.log(String(event.target)) )
+suite.on("complete", () => console.log("Fastest is " + suite.filter("fastest").map("name")) )
+
+// run benchmark
+console.log("number array")
+suite.run({ async: false })
+
+/* ************************************************************************* */
 
 export {};
